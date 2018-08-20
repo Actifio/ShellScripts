1ffa672c58799c071842515afd71469443b43be5
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2018-08-20 15:26:04 +1000

    Update healthcheck.sh

--- EmailCallHome/healthcheck.sh
+++ EmailCallHome/healthcheck.sh
@@ -4,13 +4,21 @@
 # This script collects health checks
 # Version 1.0 Initial Release
 
+# Now check for inputs 
+while getopts :f opt
+do
+        case "$opt"
+        in
+                f) fileonly=y;;
+        esac
+done
+
 ################################################################################################
 ################   THE SECTION BELOW NEEDS TO BE CUSTOMIZED.   LEAVING AS DEFAULT WILL NOT WORK!
 ################################################################################################
 #user name on each appliance.   You define this on each Actifio Appliance and use CLI to access
 username=admin
 
-# key location on the RM server.
 # It is the private key that matches the public key assigned to every user on every Actifio Appliance
 userkey=/root/.ssh/id_rsa
 
@@ -49,15 +57,36 @@ echo "Actifo Report created on ${currentdate}" > /root/$reportname
 echo "--------------------------------------------------------------------------------" >> /root/$reportname
 
 # work the list
-echo "$clusterlist" | while IFS="," read -ra cluster; do
-	reporthealthout=$(ssh -n -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $userkey $username@${cluster[1]} reporthealth 2>/dev/null)
-	if [ $? -ne 0 ]; then
-		 echo "Failed to SSH to ${cluster[0]} using IP address ${cluster[1]} with username $username" >> /root/$reportname
-	else
-		 echo "$reporthealthout" >> /root/$reportname
-	fi
-	echo "--------------------------------------------------------------------------------" >> /root/$reportname
+echo "$clusterlist" | { while IFS="," read -ra cluster; do
+	reporthealthout=$(ssh -n -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $userkey $username@${cluster[1]} "reporthealth -flncm" 2>/dev/null)
+	[[ -z "$reporthealthout" ]] && echo "Failed to SSH to ${cluster[0]} using IP address ${cluster[1]} with username $username" >> /root/$reportname
+	[[ $(echo "$reporthealthout" | wc -l) -eq 2 ]] && let cleancount+=1 && cleanlist=$(echo "$reporthealthout" | tail -1 | awk -F"," '{ print $1 }' | awk -F":" '{ print $2 }'; echo "$cleanlist") && continue
+	colput=$(echo "$reporthealthout" | head -2| tail -1;echo >> /root/$reportname;echo "$reporthealthout" | head -1;echo "$reporthealthout" | tail -n +3;echo "-----------------------------,-----------------------------,---------------"; echo "$colput")
 done
+echo "$colput" | column -t -s, >> /root/$reportname
+# Create the final report:
+[[ $clustercount -gt $cleancount ]] && echo "Clean Appliances were:"  >> /root/$reportname
+[[ $clustercount -eq $cleancount ]] && echo "There were no clean Appliances"  >> /root/$reportname
+echo "$cleanlist" | sort >> /root/$reportname
+
+[[ $clustercount -ne $cleancount ]] &&  sed -i "1i$clustercount Appliances were checked and $cleancount Clean Appliances were found.  Appliances with messages are displayed below:" /root/$reportname
+[[ $clustercount -eq $cleancount ]] &&  sed -i "1i$clustercount Appliances were checked and $cleancount Clean Appliances were found." /root/$reportname
+sed -i "1iReport from Actifio Report Manager created on ${currentdate}" /root/$reportname
+}
+# the close bracket above is part of a closed loop, don't remove it
+
+# clean blank lines
+sed -i '/^$/d' /root/$reportname
+
+# clean old report files that are older than one week
+find /root -name "reporthealth_*.txt" -mtime +8 -exec rm -f {} \;
+
+#  Execute file only,  print and exit without mailing
+if [ "$fileonly" == "y" ]; then
+	cat /root/$reportname
+	exit 0
+fi
+
 
 # use sendmail
 (echo "To: $recipient"
@@ -79,6 +108,3 @@ echo '</pre>
 
 --=_myboundary--') | /usr/sbin/sendmail -F "" -f $emailuser "$recipient"
 
-
-# clean old report files that are older than one week
-find /root -name "reporthealth_*.txt" -mtime +8 -exec rm -f {} \;
